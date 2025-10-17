cf6f1d73558780b97c9d51421e39eeb73335c0ed
FabioTucciarone <fabiotucciarone@gmail.com>
2025-10-17 17:56:06 +0200

    Versionen & Bibliotheken angepasst

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+#ifdef VTK_AVAILABLE
+#include <vtkMath.h>
+#endif
+
 int main() {
     std::cout << "Hi" << std::endl;
+
+#ifdef VTK_AVAILABLE
+    double p0[3] = {0.0, 0.0, 0.0};
+    double p1[3] = {1.0, 1.0, 1.0};
+    double squaredDistance = vtkMath::Distance2BetweenPoints(p0, p1);
+    std::cout << "vtkMath::Distance2BetweenPoints(p0, p1) = " << squaredDistance << std::endl;
+#endif
 }
