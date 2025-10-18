275f81402cd83e89b4683ffd27d4d57913cdf5b8
julianschmoll <dev@julianschmoll.de>
2025-10-18 20:48:34 +0200

    add paraview output

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,12 @@
+#include "output_writer/write_paraview_output.h"
+
 #include <iostream>
+#include <cstdlib>
 
-#ifdef VTK_AVAILABLE
-#include <vtkMath.h>
-#endif
-
-int main() {
-    std::cout << "Hi" << std::endl;
-
-#ifdef VTK_AVAILABLE
-    double p0[3] = {0.0, 0.0, 0.0};
-    double p1[3] = {1.0, 1.0, 1.0};
-    double squaredDistance = vtkMath::Distance2BetweenPoints(p0, p1);
-    std::cout << "vtkMath::Distance2BetweenPoints(p0, p1) = " << squaredDistance << std::endl;
-#endif
+int main(int argc, char *argv[]) {
+    // write 5 output files
+    for (int i = 0; i < 5; i++) {
+        writeParaviewOutput(i);
+    }
+    return EXIT_SUCCESS;
 }
