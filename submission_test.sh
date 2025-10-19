167007d7d692778e92249dddeb58ec4271087214
julianschmoll <dev@julianschmoll.de>
2025-10-19 21:55:07 +0200

    just dirty for now

---
+++ submission_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# unzip submission.zip                    # this unzips the uploaded archive, filename may be different
+mkdir -p build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make install
+./numsim parameters.txt
+# instead of lid_driven_cavity.txt, other files will be tested and should produce the respective results
