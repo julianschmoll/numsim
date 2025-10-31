b55f8c97fb75e74cea81d30489f860b26e130d0a
FabioTucciarone <fabiotucciarone@gmail.com>
2025-10-31 16:25:31 +0100

    more understandable variable names in central differences

--- src/simulation/velocity/centralDifferences.cpp
+++ src/simulation/velocity/centralDifferences.cpp
@@ -5,33 +5,35 @@ centralDifferences::centralDifferences(const std::array<int, 2> &nCells, const s
 { }
 
 double centralDifferences::computeDu2Dx(int i, int j) const {
-    const double u1 = (u_(i + 1, j) + u_(i, j)) / 2;
-    const double u2 = (u_(i - 1, j) + u_(i, j)) / 2;
-    return (u1 * u1 - u2 * u2) / dx();
+    const double uHalfRight = (u_(i + 1, j) + u_(i, j)) / 2;
+    const double uHalfLeft  = (u_(i - 1, j) + u_(i, j)) / 2;
+
+    return (uHalfRight * uHalfRight - uHalfLeft * uHalfLeft) / dx();
 }
 
 double centralDifferences::computeDv2Dy(int i, int j) const {
-    const double v1 = (v_(i, j + 1) + v_(i, j)) / 2;
-    const double v2 = (v_(i, j - 1) + v_(i, j)) / 2;
-    return (v1 * v1 - v2 * v2) / dy();
+    const double vHalfUp   = (v_(i, j + 1) + v_(i, j)) / 2;
+    const double vHalfDown = (v_(i, j - 1) + v_(i, j)) / 2;
+
+    return (vHalfUp * vHalfUp - vHalfDown * vHalfDown) / dy();
 }
 
 double centralDifferences::computeDuvDx(int i, int j) const {
-    const double v1 = (v_(i + 1, j) + v_(i, j)) / 2;
-    const double v2 = (v_(i - 1, j) + v_(i, j)) / 2;
+    const double vHalfRight = (v_(i + 1, j) + v_(i, j)) / 2;
+    const double vHalfLeft  = (v_(i - 1, j) + v_(i, j)) / 2;
 
-    const double u1 = (u_(i, j + 1) + u_(i, j)) / 2;
-    const double u2 = (u_(i - 1, j + 1) + u_(i - 1, j)) / 2;
+    const double uHalfUp     = (u_(i, j + 1) + u_(i, j)) / 2;
+    const double uHalfUpLeft = (u_(i - 1, j + 1) + u_(i - 1, j)) / 2;
     
-    return (v1 * u1 - v2 * u2) / dx();
+    return (vHalfRight * uHalfUp - vHalfLeft * uHalfUpLeft) / dx();
 }
 
 double centralDifferences::computeDuvDy(int i, int j) const {
-    const double v1 = (v_(i + 1, j) + v_(i, j)) / 2;
-    const double v2 = (v_(i + 1, j - 1) + v_(i, j - 1)) / 2;
+    const double vHalfRight     = (v_(i + 1, j) + v_(i, j)) / 2;
+    const double vHalfRightDown = (v_(i, j - 1) + v_(i + 1, j - 1)) / 2;
 
-    const double u1 = (u_(i, j + 1) + u_(i, j)) / 2;
-    const double u2 = (u_(i, j - 1) + u_(i, j)) / 2;
-    
-    return (v1 * u1 - v2 * u2) / dy();
+    const double uHalfUp   = (u_(i, j + 1) + u_(i, j)) / 2;
+    const double uHalfDown = (u_(i, j) + u_(i, j - 1)) / 2;
+
+    return (vHalfRight * uHalfUp - vHalfRightDown * uHalfDown) / dy();
 }
