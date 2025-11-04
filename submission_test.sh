42b8bf04a4a7d6a63df3290af8be67aaff2473b9
julianschmoll <dev@julianschmoll.de>
2025-11-04 16:18:06 +0100

    remove unnecessary files

--- submission_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# unzip submission.zip                    # this unzips the uploaded archive, filename may be different
-mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make install
-./numsim ../scenarios/lid_driven_cavity.txt
-# instead of lid_driven_cavity.txt, other files will be tested and should produce the respective results
