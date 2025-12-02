947c46baae4ebf540cd5727461ee023240613e67
FabioTucciarone <fabiotucciarone@gmail.com>
2025-12-02 21:42:50 +0100

    remove unncecessary scope in main

--- src/main.cpp
+++ src/main.cpp
@@ -25,13 +25,9 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &ownRankNo);
     MPI_Comm_size(MPI_COMM_WORLD, &nRanks);
 
-    //testCode();
-    {
-        ParallelSimulation simulation{settings};
-        //simulation.initialize(settings);
-        simulation.run();
-    }
-
+    ParallelSimulation simulation{settings};
+    simulation.run();
+    
     MPI_Finalize();
 
     return EXIT_SUCCESS;
