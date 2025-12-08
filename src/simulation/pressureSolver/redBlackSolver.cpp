a44a3abcbd415c5705159488ff7557fe751a4c72
FabioTucciarone <fabiotucciarone@gmail.com>
2025-12-08 18:02:33 +0100

    Fix non-global checkerboard pattern of RB-GS.

--- src/simulation/pressureSolver/redBlackSolver.cpp
+++ src/simulation/pressureSolver/redBlackSolver.cpp
@@ -33,15 +33,19 @@ void RedBlackSolver::solve() {
     const double oneMinusOmega = 1 - omega_;
     const double omegaTimesScalingFactor = omega_ * scalingFactor;
 
+    auto [i0, j0] = partitioning_->nodeOffset();
+    const int globalOffset = (i0 + j0) % 2;
+
     while (globalPressureError_ > epsilonSquared && it < maxNumberOfIterations_) {
         localPressureError_ = 0.0;
-// rb = 0 is red, rb=1 is black pass
-#pragma omp parallel for reduction(+ : localPressureError_)
+        // rb = 0 is red, rb=1 is black pass
+        #pragma omp parallel for reduction(+ : localPressureError_)
         for (int rb = 0; rb < 2; ++rb) {
-#pragma omp simd reduction(+ : localPressureError_)
+            #pragma omp simd reduction(+ : localPressureError_)
             for (int j = beginJ; j < endJ; ++j) {
-                // ((beginI + j + rb) & 1) calculates offset 0 or 1
-                for (int i = beginI + ((beginI + j + rb) & 1); i < endI; i += 2) {
+                // offset is 0 or 1 depending on the color of the cell
+                const int offset = (beginI + rb + globalOffset + j) & 1;
+                for (int i = beginI + offset; i < endI; i += 2) {
                     const double pDxx = (p(i - 1, j) + p(i + 1, j)) * invDx2;
                     const double pDyy = (p(i, j - 1) + p(i, j + 1)) * invDy2;
                     const double pNew = oneMinusOmega * p(i, j) + omegaTimesScalingFactor * (pDxx + pDyy - rhs(i, j));
