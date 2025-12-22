4dd1ea953737e0228235f263814c075a9ee2d389
julianschmoll <dev@julianschmoll.de>
2025-12-22 10:25:28 +0100

    first implementation of model

--- fluid_ml/model.py
+++ fluid_ml/model.py
@@ -4,7 +4,35 @@ from torch import nn
 class FluidCNN(nn.Module):
     def __init__(self, config):
         super().__init__()
-        # ToDo
+        layers = [
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=7,
+                stride=1,
+                padding="same"
+            ),
+            nn.ReLU()
+        ]
 
-    def forward(self, *args):
-        pass
+        for _ in range(5):
+            layers.append(nn.Conv2d(
+                in_channels=16,
+                out_channels=16,
+                kernel_size=7,
+                stride=1,
+                padding="same"
+            ))
+            layers.append(nn.ReLU())
+
+        layers.append(nn.Conv2d(
+            in_channels=16,
+            out_channels=2,
+            kernel_size=7,
+            stride=1,
+            padding="same"
+        ))
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.net(x)
