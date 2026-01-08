c20768a7a09ed1430c67d363cc4b6eba25a898e5
julianschmoll <dev@julianschmoll.de>
2026-01-08 10:48:25 +0100

    finish the run_pipeline script

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,6 +1,46 @@
-# should run the whole ml pipeline
+#!/bin/bash
+set -e
 
-# would be nice to parse the config file as cli argument here
-# then this could build the cpp, generate training data, load the dataset
-# train the dataset (should be possible to specify random parameters so we can let this run on a cluster)
-# evaluate (ofc) and save the dataset alongside a config.json with the training parameters and a mymodel.py to initialize
+GEN_CONFIG=$(readlink -f "./cfg/ml/train.txt")
+TRAIN_CONFIG=$(readlink -f "./cfg/ml/config.json")
+
+usage() {
+    echo "Usage: $0 [-g generation_config] [-t training_config]"
+    echo ""
+    echo "Options:"
+    echo "  -g    Path to the data generation config (Default: ./cfg/ml/train.txt)"
+    echo "  -t    Path to the ML training config (Default: ./cfg/ml/config.json)"
+    echo "  -h    Display this help message"
+    exit 1
+}
+
+while getopts "g:t:h" opt; do
+  case $opt in
+    g) GEN_CONFIG=$(readlink -f "$OPTARG") ;;
+    t) TRAIN_CONFIG=$(readlink -f "$OPTARG") ;;
+    h) usage ;;
+    \?) usage ;;
+  esac
+done
+
+echo "----------------------------------------"
+echo "SIMULATION CONFIG: $GEN_CONFIG"
+echo "ML CONFIG:         $TRAIN_CONFIG"
+echo "----------------------------------------"
+
+echo "----------------------------------------"
+echo "Building and Running Simulations..."
+echo "----------------------------------------"
+
+rm -rf build
+mkdir -p build && cd build
+cmake ..
+make install
+mpirun numsim_parallel "$GEN_CONFIG"
+cd ..
+
+echo "----------------------------------------"
+echo "Training ML Model..."
+echo "----------------------------------------"
+
+python ./fluid_ml/main.py --config "$TRAIN_CONFIG"
