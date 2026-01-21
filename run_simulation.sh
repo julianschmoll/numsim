7e41b2d69cef7d8c27c60c64104553a0a4f30866
julianschmoll <dev@julianschmoll.de>
2026-01-21 10:57:02 +0100

    add calculix cfg files and restructure

--- run_simulation.sh
+++ run_simulation.sh
@@ -4,4 +4,4 @@ rm -rf ./solver/build/
 mkdir -p ./solver/build && cd ./solver/build
 cmake ..
 make install
-mpirun ./numsim_parallel ../../cfg/scenarios/lid_driven_cavity.txt
+mpirun ./numsim_parallel ../../cfg/fluid/lid_driven_cavity.txt
